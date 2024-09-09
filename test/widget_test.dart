ad8544c00a1dd855914050a626a6378be1d5ab27
geusan <dnay2k@gmail.com>
2024-09-10 00:55:30 +0900

    mod widget_test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_clean_arch/screens/home/home_screen.dart';
+import 'package:flutter_clean_arch/screens/home/home.screen.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
